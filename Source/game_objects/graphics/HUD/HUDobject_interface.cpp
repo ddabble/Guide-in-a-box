3531c3e7a17404bf0a3913f8ed2fa2a21d3dee49
Dabble <andershovden@protonmail.com>
2017-08-11 00:16:26 +0200

    HUDobject works better with HUDobject_Animated
    Brought back the underscore-prefixed static functions, since they make
    the functions in HUDobject_Animated a lot easier to write in regard to
    memory management.
    
    Also renamed setFields() to setCoords(), and switched out some int
    parameters with float ones, to allow the user to choose.

--- Source/game_objects/graphics/HUD/HUDobject_interface.cpp
+++ Source/game_objects/graphics/HUD/HUDobject_interface.cpp
@@ -12,6 +12,22 @@ static constexpr GLfloat VERTEX_TEMPLATE[] =
 };
 
 HUDobject_interface::HUDobject_interface(GLuint program, const GraphicsObjectManager& graphicsObjectManager)
+	: HUDobject_interface(program, graphicsObjectManager, 0.0f, 0.0f, (GLfloat)graphicsObjectManager.getWindow().getWidth(), (GLfloat)graphicsObjectManager.getWindow().getHeight()) {}
+
+HUDobject_interface::HUDobject_interface(GLuint program, const GraphicsObjectManager& graphicsObjectManager, GLfloat xPos, GLfloat yPos, GLfloat width, GLfloat height)
+	: m_vertexData
+{
+	// Vertices
+	xPos,         yPos,
+	xPos + width, yPos,
+	xPos + width, yPos + height,
+	xPos,         yPos + height,
+	// Texture coordinates
+	0, 0,
+	1, 0,
+	1, 1,
+	0, 1
+}
 {
 	glGenVertexArrays(1, &m_vertexArrayObject);
 	glBindVertexArray(m_vertexArrayObject);
@@ -40,104 +56,91 @@ void HUDobject_interface::graphicsUpdate(GLuint program, const GraphicsObjectMan
 	glDrawArrays(GL_TRIANGLE_FAN, 0, 4);
 }
 
-void HUDobject_interface::setFields(GLint width, GLint height, GLint xPos, GLint yPos)
+void HUDobject_interface::_setCoords(GLfloat xPos, GLfloat yPos, GLfloat width, GLfloat height, GLfloat* vertexData)
 {
-	GLfloat vertexData[] =
+	const GLfloat newVertexData[] =
 	{
-		// Vertices
 		xPos,         yPos,
 		xPos + width, yPos,
 		xPos + width, yPos + height,
 		xPos,         yPos + height,
-		// Texture coordinates
-		0, 0,
-		1, 0,
-		1, 1,
-		0, 1
 	};
 
-	std::memcpy(m_vertexData, vertexData, sizeof(vertexData));
+	std::memcpy(vertexData, newVertexData, sizeof(newVertexData));
 }
 
-void HUDobject_interface::setWidth(GLint width, bool preserveAspectRatio)
+void HUDobject_interface::_setWidth(GLfloat width, bool preserveAspectRatio, GLfloat* vertexData)
 {
 	if (preserveAspectRatio)
 	{
-		GLfloat aspectRatio = (m_vertexData[2] - m_vertexData[0]) / (m_vertexData[5] - m_vertexData[1]);
+		GLfloat aspectRatio = _getWidth(vertexData) / _getHeight(vertexData);
 		GLfloat newHeight = width / aspectRatio;
 
-		m_vertexData[5] = m_vertexData[1] + newHeight;
-		m_vertexData[7] = m_vertexData[1] + newHeight;
+		vertexData[5] = vertexData[1] + newHeight;
+		vertexData[7] = vertexData[1] + newHeight;
 	}
 
-	m_vertexData[2] = m_vertexData[0] + width;
-	m_vertexData[4] = m_vertexData[0] + width;
+	vertexData[2] = vertexData[0] + width;
+	vertexData[4] = vertexData[0] + width;
 }
 
-void HUDobject_interface::setHeight(GLint height, bool preserveAspectRatio)
+void HUDobject_interface::_setHeight(GLfloat height, bool preserveAspectRatio, GLfloat* vertexData)
 {
 	if (preserveAspectRatio)
 	{
-		GLfloat aspectRatio = (m_vertexData[2] - m_vertexData[0]) / (m_vertexData[5] - m_vertexData[1]);
+		GLfloat aspectRatio = _getWidth(vertexData) / _getHeight(vertexData);
 		GLfloat newWidth = height * aspectRatio;
 
-		m_vertexData[2] = m_vertexData[0] + newWidth;
-		m_vertexData[4] = m_vertexData[0] + newWidth;
+		vertexData[2] = vertexData[0] + newWidth;
+		vertexData[4] = vertexData[0] + newWidth;
 	}
 
-	m_vertexData[5] = m_vertexData[1] + height;
-	m_vertexData[7] = m_vertexData[1] + height;
+	vertexData[5] = vertexData[1] + height;
+	vertexData[7] = vertexData[1] + height;
 }
 
-void HUDobject_interface::move(GLint xDirection, GLint yDirection)
+void HUDobject_interface::_move(GLfloat xDirection, GLfloat yDirection, GLfloat* vertexData)
 {
 	for (int i = 0; i < 8; i += 2)
 	{
-		m_vertexData[i] += xDirection;
-		m_vertexData[i + 1] += yDirection;
+		vertexData[i] += xDirection;
+		vertexData[i + 1] += yDirection;
 	}
 }
 
-void HUDobject_interface::moveTo(GLint xPos, GLint yPos)
+void HUDobject_interface::_moveTo(GLfloat xPos, GLfloat yPos, GLfloat* vertexData)
 {
-	GLint windowWidth = getWidth();
-	GLint windowHeight = getHeight();
-
-	for (int i = 0; i < 8; i += 2)
-	{
-		m_vertexData[i] = xPos + VERTEX_TEMPLATE[i] * windowWidth;
-		m_vertexData[i + 1] = yPos + VERTEX_TEMPLATE[i + 1] * windowHeight;
-	}
+	_setCoords(xPos, yPos, _getWidth(vertexData), _getHeight(vertexData), vertexData);
 }
 
-void HUDobject_interface::zoom(GLint newWidth, GLint newHeight, GLfloat focusX, GLfloat focusY)
+void HUDobject_interface::_zoom(GLfloat newWidth, GLfloat newHeight, GLfloat focusX, GLfloat focusY, GLfloat* vertexData)
 {
 	focusX = glm::clamp(focusX, 0.0f, 1.0f);
 	focusY = glm::clamp(focusY, 0.0f, 1.0f);
 
-	GLint oldWidth = getWidth();
-	GLint oldHeight = getHeight();
+	GLfloat oldWidth = _getWidth(vertexData);
+	GLfloat oldHeight = _getHeight(vertexData);
 
-	if (newWidth < 0)
+	if (newWidth < 0.0f)
 	{
-		if (newHeight < 0)
+		if (newHeight < 0.0f)
 			return;
 
 		newWidth = newHeight * (oldWidth / oldHeight);
 	}
 
-	if (newHeight < 0)
+	if (newHeight < 0.0f)
 		newHeight = newWidth / (oldWidth / oldHeight);
 
 	GLfloat deltaWidth = newWidth - oldWidth;
 	GLfloat deltaHeight = newHeight - oldHeight;
 
-	GLfloat cursorPosInTexCoords_x = ((focusX * 2 - 1) - m_vertexData[0]) / oldWidth;
-	GLfloat cursorPosInTexCoords_y = ((focusY * 2 - 1) - m_vertexData[1]) / oldHeight;
+	GLfloat cursorPosInTexCoords_x = ((focusX * 2 - 1) - vertexData[0]) / oldWidth;
+	GLfloat cursorPosInTexCoords_y = ((focusY * 2 - 1) - vertexData[1]) / oldHeight;
 
 	for (int i = 0; i < 8; i += 2)
 	{
-		m_vertexData[i] += (VERTEX_TEMPLATE[i] - cursorPosInTexCoords_x) * deltaWidth;
-		m_vertexData[i + 1] += (VERTEX_TEMPLATE[i + 1] - cursorPosInTexCoords_y) * deltaHeight;
+		vertexData[i] += (VERTEX_TEMPLATE[i] - cursorPosInTexCoords_x) * deltaWidth;
+		vertexData[i + 1] += (VERTEX_TEMPLATE[i + 1] - cursorPosInTexCoords_y) * deltaHeight;
 	}
 }
--- Source/game_objects/graphics/HUD/HUDobject_interface.h
+++ Source/game_objects/graphics/HUD/HUDobject_interface.h
@@ -9,14 +9,16 @@
 class HUDobject_interface
 {
 protected:
-	GLfloat m_vertexData[4 * 2 + 4 * 2];
-
 	GLuint m_vertexArrayObject;
 
 	GLint m_vertexData_uniformIndex;
+	GLfloat m_vertexData[4 * 2 + 4 * 2];
 
 protected:
+	/* User should call setCoords() after calling this constructor. */
 	HUDobject_interface(GLuint program, const GraphicsObjectManager& graphicsObjectManager);
+
+	HUDobject_interface(GLuint program, const GraphicsObjectManager& graphicsObjectManager, GLfloat xPos, GLfloat yPos, GLfloat width, GLfloat height);
 
 public:
 	virtual ~HUDobject_interface() {}
@@ -26,25 +28,37 @@ public:
 
 protected:
 	/*
-	width and height are the image's dimensions measured in pixels.
 	xPos and yPos are the coordinates of the image's lower left corner.
+	width and height are the image's dimensions measured in pixels.
 	*/
-	virtual void setFields(GLint width, GLint height, GLint xPos, GLint yPos);
+	virtual void setCoords(GLfloat xPos, GLfloat yPos, GLfloat width, GLfloat height) { _setCoords(xPos, yPos, width, height, m_vertexData); }
 
-	GLint getWidth() { return m_vertexData[2] - m_vertexData[0]; }
-	GLint getHeight() { return m_vertexData[5] - m_vertexData[3]; }
+	virtual GLfloat getWidth() const { return _getWidth(m_vertexData); }
+	virtual GLfloat getHeight() const { return _getHeight(m_vertexData); }
 
-	virtual void setWidth(GLint width, bool preserveAspectRatio);
-	virtual void setHeight(GLint height, bool preserveAspectRatio);
+	virtual void setWidth(GLfloat width, bool preserveAspectRatio) { _setWidth(width, preserveAspectRatio, m_vertexData); }
+	virtual void setHeight(GLfloat height, bool preserveAspectRatio) { _setHeight(height, preserveAspectRatio, m_vertexData); }
 
-	virtual void move(GLint xDirection, GLint yDirection);
+	virtual void move(GLfloat xDirection, GLfloat yDirection) { _move(xDirection, yDirection, m_vertexData); }
 
 	/* xPos and yPos are the coordinates of the image's lower left corner. */
-	virtual void moveTo(GLint xPos, GLint yPos);
+	virtual void moveTo(GLfloat xPos, GLfloat yPos) { _moveTo(xPos, yPos, m_vertexData); }
 
 	/*
 	Set newWidth or newHeight to -1 to make the image maintain its aspect ratio during the zoom.
 	focusX and focusY are relative to the window's lower left corner, and are clamped between 0 and 1.
 	*/
-	virtual void zoom(GLint newWidth, GLint newHeight, GLfloat focusX = 0.5f, GLfloat focusY = 0.5f);
+	virtual void zoom(GLfloat newWidth, GLfloat newHeight, GLfloat focusX = 0.5f, GLfloat focusY = 0.5f) { _zoom(newWidth, newHeight, focusX, focusY, m_vertexData); }
+
+protected:
+	static void _setCoords(GLfloat xPos, GLfloat yPos, GLfloat width, GLfloat height, GLfloat vertexData[8]);
+
+	constexpr static GLfloat _getWidth(const GLfloat vertexData[8]) { return vertexData[2] - vertexData[0]; }
+	constexpr static GLfloat _getHeight(const GLfloat vertexData[8]) { return vertexData[5] - vertexData[3]; }
+
+	static void _setWidth(GLfloat width, bool preserveAspectRatio, GLfloat vertexData[8]);
+	static void _setHeight(GLfloat height, bool preserveAspectRatio, GLfloat vertexData[8]);
+	static void _move(GLfloat xDirection, GLfloat yDirection, GLfloat vertexData[8]);
+	static void _moveTo(GLfloat xPos, GLfloat yPos, GLfloat vertexData[8]);
+	static void _zoom(GLfloat newWidth, GLfloat newHeight, GLfloat focusX, GLfloat focusY, GLfloat vertexData[8]);
 };
