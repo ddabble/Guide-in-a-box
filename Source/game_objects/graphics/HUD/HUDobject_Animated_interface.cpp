da099587722eb99ff8b73161221df75c1770ad60
Dabble <ddabble@users.noreply.github.com>
2017-08-14 02:47:45 +0200

    Condensed repeated animation code
    Not sure if I like it or not, but at least it's much more manageable.

--- Source/game_objects/graphics/HUD/HUDobject_Animated_interface.cpp
+++ Source/game_objects/graphics/HUD/HUDobject_Animated_interface.cpp
@@ -32,131 +32,41 @@ void HUDobject_Animated_interface::graphicsUpdate(GLuint program, const Graphics
 
 void HUDobject_Animated_interface::setCoords(glm::vec2 pos, GLfloat width, GLfloat height, float animationDuration)
 {
-	if (animationDuration > 0.0f)
-	{
-		m_animationStartTime = glfwGetTime();
-		if (!m_isAnimating)
-		{
-			std::memcpy(m_vertexDataAnimationOrigin, m_vertexData, sizeof(m_vertexDataAnimationOrigin));
-			std::memcpy(m_vertexDataAnimationDestination, m_vertexData, sizeof(m_vertexDataAnimationDestination));
-			_setCoords(pos, width, height, m_vertexDataAnimationDestination);
-
-			m_isAnimating = true;
-		} else
-			_setCoords(pos, width, height, m_vertexDataAnimationDestination);
-
-		m_animationEndTime = m_animationStartTime + animationDuration;
-
-	} else if (m_isAnimating)
-	{
-		_setCoords(pos, width, height, m_vertexDataAnimationOrigin);
-		_setCoords(pos, width, height, m_vertexDataAnimationDestination);
-	} else
-		_setCoords(pos, width, height, m_vertexData);
+	void* args[]{ &pos, &width, &height };
+	animate(SET_COORDS, args, animationDuration);
 }
 
 void HUDobject_Animated_interface::setWidth(GLfloat width, float animationDuration)
 {
-	if (animationDuration > 0.0f)
-	{
-		m_animationStartTime = glfwGetTime();
-		if (!m_isAnimating)
-		{
-			std::memcpy(m_vertexDataAnimationOrigin, m_vertexData, sizeof(m_vertexDataAnimationOrigin));
-			std::memcpy(m_vertexDataAnimationDestination, m_vertexData, sizeof(m_vertexDataAnimationDestination));
-			_setWidth(width, m_vertexDataAnimationDestination);
-
-			m_isAnimating = true;
-		} else
-			_setWidth(width, m_vertexDataAnimationDestination);
-
-		m_animationEndTime = m_animationStartTime + animationDuration;
-
-	} else if (m_isAnimating)
-	{
-		_setWidth(width, m_vertexDataAnimationOrigin);
-		_setWidth(width, m_vertexDataAnimationDestination);
-	} else
-		_setWidth(width, m_vertexData);
+	void* args[]{ &width };
+	animate(SET_WIDTH, args, animationDuration);
 }
 
 void HUDobject_Animated_interface::setHeight(GLfloat height, float animationDuration)
 {
-	if (animationDuration > 0.0f)
-	{
-		m_animationStartTime = glfwGetTime();
-
-		if (!m_isAnimating)
-		{
-			std::memcpy(m_vertexDataAnimationOrigin, m_vertexData, sizeof(m_vertexDataAnimationOrigin));
-			std::memcpy(m_vertexDataAnimationDestination, m_vertexData, sizeof(m_vertexDataAnimationDestination));
-			_setHeight(height, m_vertexDataAnimationDestination);
-
-			m_isAnimating = true;
-		} else
-			_setHeight(height, m_vertexDataAnimationDestination);
-
-		m_animationEndTime = m_animationStartTime + animationDuration;
-
-	} else if (m_isAnimating)
-	{
-		_setHeight(height, m_vertexDataAnimationOrigin);
-		_setHeight(height, m_vertexDataAnimationDestination);
-	} else
-		_setHeight(height, m_vertexData);
+	void* args[]{ &height };
+	animate(SET_HEIGHT, args, animationDuration);
 }
 
 void HUDobject_Animated_interface::move(glm::vec2 direction, float animationDuration)
 {
-	if (animationDuration > 0.0f)
-	{
-		m_animationStartTime = glfwGetTime();
-		if (!m_isAnimating)
-		{
-			std::memcpy(m_vertexDataAnimationOrigin, m_vertexData, sizeof(m_vertexDataAnimationOrigin));
-			std::memcpy(m_vertexDataAnimationDestination, m_vertexData, sizeof(m_vertexDataAnimationDestination));
-			_move(direction, m_vertexDataAnimationDestination);
-
-			m_isAnimating = true;
-		} else
-			_move(direction, m_vertexDataAnimationDestination);
-
-		m_animationEndTime = m_animationStartTime + animationDuration;
-
-	} else if (m_isAnimating)
-	{
-		_move(direction, m_vertexDataAnimationOrigin);
-		_move(direction, m_vertexDataAnimationDestination);
-	} else
-		_move(direction, m_vertexData);
+	void* args[]{ &direction };
+	animate(MOVE, args, animationDuration);
 }
 
 void HUDobject_Animated_interface::moveTo(glm::vec2 pos, float animationDuration)
 {
-	if (animationDuration > 0.0f)
-	{
-		m_animationStartTime = glfwGetTime();
-		if (!m_isAnimating)
-		{
-			std::memcpy(m_vertexDataAnimationOrigin, m_vertexData, sizeof(m_vertexDataAnimationOrigin));
-			std::memcpy(m_vertexDataAnimationDestination, m_vertexData, sizeof(m_vertexDataAnimationDestination));
-			_moveTo(pos, m_vertexDataAnimationDestination);
-
-			m_isAnimating = true;
-		} else
-			_moveTo(pos, m_vertexDataAnimationDestination);
-
-		m_animationEndTime = m_animationStartTime + animationDuration;
-
-	} else if (m_isAnimating)
-	{
-		_moveTo(pos, m_vertexDataAnimationOrigin);
-		_moveTo(pos, m_vertexDataAnimationDestination);
-	} else
-		_moveTo(pos, m_vertexData);
+	void* args[]{ &pos };
+	animate(MOVE_TO, args, animationDuration);
 }
 
 void HUDobject_Animated_interface::zoom(GLfloat newWidth, GLfloat newHeight, float animationDuration, glm::vec2 focus)
+{
+	void* args[]{ &newWidth, &newHeight, &focus };
+	animate(ZOOM, args, animationDuration);
+}
+
+void HUDobject_Animated_interface::animate(animationFunction func, void* args[], float animationDuration)
 {
 	if (animationDuration > 0.0f)
 	{
@@ -165,18 +75,48 @@ void HUDobject_Animated_interface::zoom(GLfloat newWidth, GLfloat newHeight, flo
 		{
 			std::memcpy(m_vertexDataAnimationOrigin, m_vertexData, sizeof(m_vertexDataAnimationOrigin));
 			std::memcpy(m_vertexDataAnimationDestination, m_vertexData, sizeof(m_vertexDataAnimationDestination));
-			_zoom(newWidth, newHeight, focus, m_vertexDataAnimationDestination);
+			invoke(func, args, m_vertexDataAnimationDestination);
 
 			m_isAnimating = true;
 		} else
-			_zoom(newWidth, newHeight, focus, m_vertexDataAnimationDestination);
+			invoke(func, args, m_vertexDataAnimationDestination);
 
 		m_animationEndTime = m_animationStartTime + animationDuration;
 
 	} else if (m_isAnimating)
 	{
-		_zoom(newWidth, newHeight, focus, m_vertexDataAnimationOrigin);
-		_zoom(newWidth, newHeight, focus, m_vertexDataAnimationDestination);
+		invoke(func, args, m_vertexDataAnimationOrigin);
+		invoke(func, args, m_vertexDataAnimationDestination);
 	} else
-		_zoom(newWidth, newHeight, focus, m_vertexData);
+		invoke(func, args, m_vertexData);
+}
+
+constexpr void HUDobject_Animated_interface::invoke(animationFunction func, void* args[], GLfloat vertexData[])
+{
+	switch (func)
+	{
+		case SET_COORDS:
+			_setCoords(*static_cast<glm::vec2*>(args[0]), *static_cast<GLfloat*>(args[1]), *static_cast<GLfloat*>(args[2]), vertexData);
+			break;
+
+		case SET_WIDTH:
+			_setWidth(*static_cast<GLfloat*>(args[0]), vertexData);
+			break;
+
+		case SET_HEIGHT:
+			_setHeight(*static_cast<GLfloat*>(args[0]), vertexData);
+			break;
+
+		case MOVE:
+			_move(*static_cast<glm::vec2*>(args[0]), vertexData);
+			break;
+
+		case MOVE_TO:
+			_moveTo(*static_cast<glm::vec2*>(args[0]), vertexData);
+			break;
+
+		case ZOOM:
+			_zoom(*static_cast<GLfloat*>(args[0]), *static_cast<GLfloat*>(args[1]), *static_cast<glm::vec2*>(args[2]), vertexData);
+			break;
+	}
 }
