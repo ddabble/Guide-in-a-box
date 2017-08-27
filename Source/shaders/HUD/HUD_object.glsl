5f045a8b3911c92f82140e6b5710c9cf8f0f4beb
Dabble <ddabble@users.noreply.github.com>
2017-08-27 02:14:26 +0200

    Index drawn -> per-vertex drawn
    Replaced the makeshift way of drawing by using uniform indices with
    drawing in a standard per-vertex way.

--- Source/shaders/HUD/HUD_object.glsl
+++ Source/shaders/HUD/HUD_object.glsl
@@ -4,17 +4,17 @@
 //layout(location = 0) in vec4 position;
 //layout(location = 1) in vec2 in_tex_coord;
 
-uniform vec2[8] vertexData;
 uniform mat4 projection;
 
-layout(location = 0) in int i;
+layout(location = 0) in vec4 vert;
+layout(location = 1) in vec2 texCoord;
 
-out vec2 texCoord;
+out vec2 vs_texCoord;
 
 void main()
 {
-	gl_Position = projection * vec4(vertexData[i], 0, 1);
-	texCoord = vertexData[4 + i];
+	gl_Position = projection * vert;
+	vs_texCoord = texCoord;
 
 	//gl_PointSize = 200.0;
 }
@@ -27,13 +27,13 @@ uniform vec3 rainbow;
 //uniform sampler2DArray tex;
 
 uniform sampler2D tex;
-in vec2 texCoord;
+in vec2 vs_texCoord;
 
 out vec4 color;
 
 void main()
 {
-	color = texture(tex, texCoord);
+	color = texture(tex, vs_texCoord);
 
 	//color = texture(tex, vec3(vs_tex_coord, 0));
 	//color = vec4(rainbow, 1.0);
