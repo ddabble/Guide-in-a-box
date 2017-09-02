0198f2980914162057b0699fc2a57b75e3c4c1e3
Dabble <ddabble@users.noreply.github.com>
2017-09-03 01:57:24 +0200

    ImageDecompression cleanup
    Also added documentation.

--- Source/texture/ImageDecompression.h
+++ Source/texture/ImageDecompression.h
@@ -2,6 +2,11 @@
 
 #include "../util/graphics/gl.h"
 
+/*
+7z file must contain only one image file.
+Returns an array of 8-bit pixel components (unsigned chars) where the first pixel is the bottom left.
+Depending on the image format, either 3 (RGB) or 4 (RGBA) components make up one whole pixel.
+*/
 unsigned char* extractImageFrom7zFile(char* file, int* width, int* height, GLenum* format);
 
 void freeImageData(unsigned char* imageData);
