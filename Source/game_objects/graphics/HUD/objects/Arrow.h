c5fa444b47c3c083344906e48fd87a334cdad1e7
Dabble <andershovden@protonmail.com>
2017-09-19 18:25:58 +0200

    Arrow updatePosition()

--- Source/game_objects/graphics/HUD/objects/Arrow.h
+++ Source/game_objects/graphics/HUD/objects/Arrow.h
@@ -2,8 +2,9 @@
 
 #include <glm/glm.hpp>
 
-#include "../../../../util/graphics/gl.h"
 #include "../../../../event/types/FramebufferSizeHook_interface.h"
+
+#include "../../../../util/graphics/gl.h"
 
 class Map;
 
@@ -16,14 +17,21 @@ private:
 
 	GLint m_projection_uniformIndex;
 
+	const glm::vec2 m_mapStartPoint;
+	const glm::vec2 m_mapEndPoint;
+
+	const GLint m_lineWidth;
+
 private:
-	void makeVertices(glm::vec2 startPoint, glm::vec2 endPoint, float lineWidth);
+	void makeVertices(const Map& map, GLint lineWidth);
 
 public:
-	Arrow(const GraphicsObjectManager& graphicsObjectManager, const Map& map, glm::vec2 mapStartPoint, glm::vec2 mapEndPoint, float lineWidth = 0.025f);
+	Arrow(const GraphicsObjectManager& graphicsObjectManager, const Map& map, glm::vec2 mapStartPoint, glm::vec2 mapEndPoint, GLint lineWidth = 10);
 	~Arrow();
 
 	void graphicsUpdate(const GraphicsObjectManager& graphicsObjectManager);
+	void updatePosition(const Map& map);
+
 
 	void framebufferSizeCallback(int lastWidth, int lastHeight, int newWidth, int newHeight, const GraphicsObjectManager& graphicsObjectManager) override;
 };
